c1d0389c476e20058059e613d00a4da002a897a8
d.kadoguchi <a.sadohara@kqrm.co.jp>
2020-02-23 14:34:11 +0900

    TODO No229

--- AirbagApp/ImageChecker/TargetSelection.cs
+++ AirbagApp/ImageChecker/TargetSelection.cs
@@ -200,7 +200,7 @@ namespace ImageChecker
                            ON  iih.product_name = mpi.product_name
                            WHERE (iih.decision_end_datetime IS NULL OR
                                   iih.decision_end_datetime >= TO_TIMESTAMP('" + strBefore48hourYmdhms + @"','YYYY/MM/DD HH24:MI:SS'))
-                           ORDER BY iih.inspection_date ASC, iih.inspection_num ASC";
+                           ORDER BY iih.inspection_date DESC, iih.inspection_num DESC";
 
                 // SQLコマンドに各パラメータを設定する
                 List<ConnectionNpgsql.structParameter> lstNpgsqlCommand = new List<ConnectionNpgsql.structParameter>();
