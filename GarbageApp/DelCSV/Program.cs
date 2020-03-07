512154e634b4249a78b2b6fb49b2d216aee2149b
k.shimoyoshi <a.sadohara@kqrm.co.jp>
2020-03-07 15:04:30 +0900

    運用ツール追加

---
+++ GarbageApp/DelCSV/Program.cs
@@ -0,0 +1,175 @@
+﻿using DelCSV.DTO;
+using log4net;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DelCSV
+{
+    static class Program
+    {
+        // 接続情報
+        public static string g_strConnectionString = string.Empty;
+        private const string g_CON_CONNECTION_STRING = "Server={0};Port={1};User ID={2};Database={3};Password={4};Enlist=true";
+        public static string g_strDBName = string.Empty;
+        public static string g_strDBUser = string.Empty;
+        public static string g_strDBUserPassword = string.Empty;
+        public static string g_strDBServerName = string.Empty;
+        public static string g_strDBPort = string.Empty;
+        public static string g_strCSVRetentionPeriod = string.Empty;
+
+        // コネクションクラス
+        public static ConnectionNpgsql g_clsConnectionNpgsql;
+
+        // システム設定情報クラス
+        public static SystemSettingInfo g_clsSystemSettingInfo;
+
+        // メッセージ情報クラス
+        public static MessageInfo g_clsMessageInfo;
+
+        // システム設定情報取得時のエラーメッセージ格納用
+        private static StringBuilder m_sbErrMessage = new StringBuilder();
+
+        // イベントログ出力関連
+        private static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public const int g_CON_LEVEL_FATAL = 1;
+        public const int g_CON_LEVEL_ERROR = 2;
+        public const int g_CON_LEVEL_WARN = 3;
+        public const int g_CON_LEVEL_INFO = 4;
+        public const int g_CON_LEVEL_DEBUG = 5;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 接続文字列をApp.configファイルから取得する。
+                GetAppConfigValue("DBName", ref g_strDBName);
+                GetAppConfigValue("DBUser", ref g_strDBUser);
+                GetAppConfigValue("DBUserPassword", ref g_strDBUserPassword);
+                GetAppConfigValue("DBServerName", ref g_strDBServerName);
+                GetAppConfigValue("DBPort", ref g_strDBPort);
+                GetAppConfigValue("CSVRetentionPeriod", ref g_strCSVRetentionPeriod);
+
+                if (m_sbErrMessage.Length > 0)
+                {
+                    // ログ出力
+                    WriteEventLog(g_CON_LEVEL_ERROR, string.Format("接続文字列取得時にエラーが発生しました。{0}{1}", Environment.NewLine, m_sbErrMessage.ToString()));
+
+                    // メッセージ出力
+                    Console.WriteLine("接続文字列取得時に例外が発生しました。");
+
+                    return;
+                }
+
+                // 接続文字列を組み立てる。
+                g_strConnectionString =
+                    string.Format(
+                        g_CON_CONNECTION_STRING,
+                        g_strDBServerName,
+                        g_strDBPort,
+                        g_strDBUser,
+                        g_strDBName,
+                        g_strDBUserPassword);
+
+                // 接続確認
+                g_clsConnectionNpgsql = new ConnectionNpgsql(g_strConnectionString);
+                g_clsConnectionNpgsql.DbOpen();
+                g_clsConnectionNpgsql.DbClose();
+
+                // システム設定情報取得
+                g_clsSystemSettingInfo = new SystemSettingInfo();
+                if (g_clsSystemSettingInfo.bolNormalEnd == false)
+                {
+                    return;
+                }
+
+                // メッセージ情報取得
+                g_clsMessageInfo = new MessageInfo();
+                if (g_clsMessageInfo.bolNormalEnd == false)
+                {
+                    return;
+                }
+
+                // 対象ディレクトリを取得
+                DirectoryInfo dyInfo = new DirectoryInfo(g_clsSystemSettingInfo.strInspectionResltCsvDirectory);
+
+                // 保持期間の取得
+                DateTime datRetentionPeriod = DateTime.Now.Date.AddMonths(-1 * int.Parse(g_strCSVRetentionPeriod));
+
+                // ファイルの取得
+                foreach (FileInfo fInfo in dyInfo.GetFiles().Where(
+                    x => string.Compare(x.Extension, ".csv", true) == 0 &&
+                    x.LastWriteTime < datRetentionPeriod))
+                {
+                    fInfo.Delete();
+                }
+            }
+            catch (Exception ex)
+            {
+                // ログ出力
+                WriteEventLog(g_CON_LEVEL_ERROR, string.Format("初期起動時にエラーが発生しました。{0}{1}", Environment.NewLine, ex.Message));
+
+                // メッセージ出力
+                Console.WriteLine("初期起動時に例外が発生しました。");
+
+                return;
+            }
+            finally
+            {
+                if (g_clsConnectionNpgsql != null)
+                {
+                    g_clsConnectionNpgsql.DbClose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// App.configファイルから設定値を取得
+        /// </summary>
+        /// <param name="strKey">キー</param>
+        /// <param name="strValue">設定値</param>
+        /// <returns>true:正常終了 false:異常終了</returns>
+        public static void GetAppConfigValue(string strKey, ref string strValue)
+        {
+            strValue = ConfigurationManager.AppSettings[strKey];
+
+            if (strValue == null)
+            {
+                m_sbErrMessage.AppendLine("Key[" + strKey + "] AppConfigに存在しません。");
+            }
+        }
+
+        /// <summary>
+        /// イベントログ出力
+        /// </summary>
+        /// <param name="intLevel">レベル</param>
+        /// <param name="strMessage">メッセージ</param>
+        public static void WriteEventLog(int intLevel, string strMessage)
+        {
+            switch (intLevel)
+            {
+                case g_CON_LEVEL_FATAL:
+                    log.Fatal(strMessage);
+                    break;
+                case g_CON_LEVEL_ERROR:
+                    log4net.ThreadContext.Properties["EventID"] = 99;
+                    log.Error(strMessage);
+                    break;
+                case g_CON_LEVEL_WARN:
+                    log4net.ThreadContext.Properties["EventID"] = 88;
+                    log.Warn(strMessage);
+                    break;
+                case g_CON_LEVEL_INFO:
+                    log4net.ThreadContext.Properties["EventID"] = 11;
+                    log.Info(strMessage);
+                    break;
+                case g_CON_LEVEL_DEBUG:
+                    log.Debug(strMessage);
+                    break;
+            }
+        }
+    }
+}
---
+++ GarbageApp/DelRecord/DTO/ConnectionNpgsql.cs
@@ -0,0 +1,250 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DelRecord
+{
+    class ConnectionNpgsql
+    {
+        // 接続文字列
+        private static string m_strConnectionString;
+        // DBコネクションオブジェクト
+        public static NpgsqlConnection NpgsqlCon;
+        // DBトランザクションオブジェクト
+        public static NpgsqlTransaction NpgsqlTran;
+
+        // パラメータ構造体
+        public struct structParameter
+        {
+            public string ParameterName;
+            public DbType DbType;
+            public object Value;
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="strConnectionString">接続文字列</param>
+        public ConnectionNpgsql(string strConnectionString)
+        {
+            m_strConnectionString = strConnectionString;
+        }
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~ConnectionNpgsql()
+        {
+            DbClose();
+        }
+
+        /// <summary>
+        /// DBオープン
+        /// </summary>
+        /// <example>既存のDBオープンが存在しない場合、DBオープンを実施する</example>
+        public void DbOpen()
+        {
+            try
+            {
+                if (NpgsqlCon == null)
+                {
+                    NpgsqlCon = new NpgsqlConnection(m_strConnectionString);
+                }
+
+                if (NpgsqlCon.FullState != System.Data.ConnectionState.Open)
+                {
+                    NpgsqlCon.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// DBクローズ
+        /// </summary>
+        /// <example>既存のDBオープンが存在しない場合、DBオープンを実施する</example>
+        public void DbClose()
+        {
+            try
+            {
+                if (NpgsqlTran != null && NpgsqlTran.IsCompleted == false)
+                {
+                    DbRollback();
+                }
+
+                if (NpgsqlCon != null && NpgsqlCon.FullState == System.Data.ConnectionState.Open)
+                {
+                    NpgsqlCon.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (NpgsqlTran != null)
+                {
+                    NpgsqlTran.Dispose();
+                    NpgsqlTran = null;
+                }
+
+                if (NpgsqlCon != null)
+                {
+                    NpgsqlCon.Dispose();
+                    NpgsqlCon = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// DBトランザクション開始
+        /// </summary>
+        public void DbBeginTran()
+        {
+            try
+            {
+                if (NpgsqlCon == null || NpgsqlCon.FullState != System.Data.ConnectionState.Open)
+                {
+                    DbOpen();
+                }
+
+                if (NpgsqlTran == null || NpgsqlTran.IsCompleted == true)
+                {
+                    NpgsqlTran = NpgsqlCon.BeginTransaction();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// DBコミット
+        /// </summary>
+        public void DbCommit()
+        {
+            try
+            {
+                if (NpgsqlTran != null && NpgsqlTran.IsCompleted == false)
+                {
+                    NpgsqlTran.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// DBロールバック
+        /// </summary>
+        public void DbRollback()
+        {
+            try
+            {
+                if (NpgsqlTran != null && NpgsqlTran.IsCompleted == false)
+                {
+                    NpgsqlTran.Rollback();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 抽出処理
+        /// </summary>
+        /// <param name="dtData">データテーブル</param>
+        /// <param name="strSQL">SQL</param>
+        /// <param name="lstNpgsqlCommand">コマンド配列</param>
+        public void SelectSQL(ref DataTable dtData, string strSQL, List<structParameter> lstNpgsqlCommand = null)
+        {
+            NpgsqlDataAdapter NpgsqlDtAd = null;
+            try
+            {
+                DbOpen();
+
+                NpgsqlDtAd = new NpgsqlDataAdapter(strSQL, NpgsqlCon);
+
+                // パラメータ引数
+                if (lstNpgsqlCommand != null)
+                {
+                    foreach (structParameter Parameter in lstNpgsqlCommand)
+                    {
+                        NpgsqlDtAd.SelectCommand.Parameters.Add(new NpgsqlParameter(Parameter.ParameterName, Parameter.DbType) { Value = Parameter.Value });
+                    }
+                }
+
+                NpgsqlDtAd.Fill(dtData);
+            }
+            catch (NpgsqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (NpgsqlTran == null || NpgsqlTran.IsCompleted == true)
+                {
+                    DbClose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 登録・更新処理実行
+        /// </summary>
+        /// <param name="strSQL">SQL</param>
+        /// <param name="lstNpgsqlCommand">コマンド配列</param>
+        public void ExecTranSQL(string strSQL, List<structParameter> lstNpgsqlCommand = null)
+        {
+            NpgsqlCommand NpgsqlCom = null;
+
+            try
+            {
+                DbOpen();
+
+                NpgsqlCom = new NpgsqlCommand(strSQL, NpgsqlCon);
+
+                NpgsqlCommand command = new NpgsqlCommand(strSQL, NpgsqlCon, NpgsqlTran);
+
+                // パラメータ引数
+                if (lstNpgsqlCommand != null)
+                {
+                    foreach (structParameter Parameter in lstNpgsqlCommand)
+                    {
+                        command.Parameters.Add(new NpgsqlParameter(Parameter.ParameterName, Parameter.DbType) { Value = Parameter.Value });
+                    }
+                }
+
+                // トランザクション開始
+                DbBeginTran();
+
+                command.ExecuteNonQuery();
+
+                // 任意のタイミングで実施してください
+                //DbCommit();
+            }
+            catch (NpgsqlException ex)
+            {
+                DbRollback();
+                DbClose();
+                throw ex;
+            }
+            finally
+            {
+                // 任意のタイミングで実施してください
+                //DbClose();
+            }
+        }
+    }
+}
