a0df6086434cd6c89dde725ce604f753e9e07b4e
k.shimoyoshi <a.sadohara@kqrm.co.jp>
2020-04-27 20:48:14 +0900

    理由選択画面のレイアウトを修正 ・合否確認、複写画面からの呼び出しは8行3列で出力 ・検査結果、履歴照会画面からの呼び出しは8行4列で出力

--- AirbagApp/ImageChecker/SelectErrorReason.Designer.cs
+++ AirbagApp/ImageChecker/SelectErrorReason.Designer.cs
@@ -38,8 +38,14 @@
             this.pnlRight = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.dgvMstDecisionReason = new ImageChecker.MyDataGridView();
-            this.Check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.Reason = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Check_Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Reason_Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Check_Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Reason_Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Check_Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Reason_Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Check_Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Reason_Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pnlBottom.SuspendLayout();
             this.panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMstDecisionReason)).BeginInit();
@@ -51,22 +57,22 @@
             this.pnlTop.Location = new System.Drawing.Point(0, 0);
             this.pnlTop.Margin = new System.Windows.Forms.Padding(2);
             this.pnlTop.Name = "pnlTop";
-            this.pnlTop.Size = new System.Drawing.Size(571, 11);
+            this.pnlTop.Size = new System.Drawing.Size(701, 11);
             this.pnlTop.TabIndex = 0;
             // 
             // pnlBottom
             // 
             this.pnlBottom.Controls.Add(this.btnOK);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 461);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 518);
             this.pnlBottom.Margin = new System.Windows.Forms.Padding(2);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(571, 70);
+            this.pnlBottom.Size = new System.Drawing.Size(701, 70);
             this.pnlBottom.TabIndex = 1;
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(202, 21);
+            this.btnOK.Location = new System.Drawing.Point(273, 21);
             this.btnOK.Margin = new System.Windows.Forms.Padding(2);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(154, 31);
@@ -81,16 +87,16 @@
             this.pnlLeft.Location = new System.Drawing.Point(0, 11);
             this.pnlLeft.Margin = new System.Windows.Forms.Padding(2);
             this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(22, 450);
+            this.pnlLeft.Size = new System.Drawing.Size(22, 507);
             this.pnlLeft.TabIndex = 2;
             // 
             // pnlRight
             // 
             this.pnlRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pnlRight.Location = new System.Drawing.Point(549, 11);
+            this.pnlRight.Location = new System.Drawing.Point(679, 11);
             this.pnlRight.Margin = new System.Windows.Forms.Padding(2);
             this.pnlRight.Name = "pnlRight";
-            this.pnlRight.Size = new System.Drawing.Size(22, 450);
+            this.pnlRight.Size = new System.Drawing.Size(22, 507);
             this.pnlRight.TabIndex = 3;
             // 
             // panel5
@@ -100,7 +106,7 @@
             this.panel5.Location = new System.Drawing.Point(22, 11);
             this.panel5.Margin = new System.Windows.Forms.Padding(2);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(527, 450);
+            this.panel5.Size = new System.Drawing.Size(657, 507);
             this.panel5.TabIndex = 4;
             // 
             // dgvMstDecisionReason
@@ -118,8 +124,14 @@
             this.dgvMstDecisionReason.ColumnHeadersHeight = 30;
             this.dgvMstDecisionReason.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dgvMstDecisionReason.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Check,
-            this.Reason});
+            this.Check_Column1,
+            this.Reason_Column1,
+            this.Check_Column2,
+            this.Reason_Column2,
+            this.Check_Column3,
+            this.Reason_Column3,
+            this.Check_Column4,
+            this.Reason_Column4});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(106)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
@@ -140,31 +152,85 @@
             this.dgvMstDecisionReason.RowTemplate.ReadOnly = true;
             this.dgvMstDecisionReason.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvMstDecisionReason.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvMstDecisionReason.Size = new System.Drawing.Size(527, 450);
+            this.dgvMstDecisionReason.Size = new System.Drawing.Size(657, 507);
             this.dgvMstDecisionReason.TabIndex = 1;
-            this.dgvMstDecisionReason.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMstDecisionReason_RowEnter);
+            this.dgvMstDecisionReason.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgvMstDecisionReason_CellBeginEdit);
+            this.dgvMstDecisionReason.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMstDecisionReason_CellEnter);
             // 
-            // Check
+            // Check_Column1
             // 
-            this.Check.HeaderText = "";
-            this.Check.MinimumWidth = 6;
-            this.Check.Name = "Check";
-            this.Check.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Check.Width = 40;
+            this.Check_Column1.HeaderText = "";
+            this.Check_Column1.MinimumWidth = 6;
+            this.Check_Column1.Name = "Check_Column1";
+            this.Check_Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Check_Column1.Width = 40;
             // 
-            // Reason
+            // Reason_Column1
             // 
-            this.Reason.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Reason.HeaderText = "理由";
-            this.Reason.MinimumWidth = 6;
-            this.Reason.Name = "Reason";
+            this.Reason_Column1.HeaderText = "理由";
+            this.Reason_Column1.MinimumWidth = 6;
+            this.Reason_Column1.Name = "Reason_Column1";
+            this.Reason_Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Reason_Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Reason_Column1.Width = 251;
+            // 
+            // Check_Column2
+            // 
+            this.Check_Column2.HeaderText = "";
+            this.Check_Column2.MinimumWidth = 6;
+            this.Check_Column2.Name = "Check_Column2";
+            this.Check_Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Check_Column2.Width = 40;
+            // 
+            // Reason_Column2
+            // 
+            this.Reason_Column2.HeaderText = "理由";
+            this.Reason_Column2.MinimumWidth = 6;
+            this.Reason_Column2.Name = "Reason_Column2";
+            this.Reason_Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Reason_Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Reason_Column2.Width = 251;
+            // 
+            // Check_Column3
+            // 
+            this.Check_Column3.HeaderText = "";
+            this.Check_Column3.MinimumWidth = 6;
+            this.Check_Column3.Name = "Check_Column3";
+            this.Check_Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Check_Column3.Width = 40;
+            // 
+            // Reason_Column3
+            // 
+            this.Reason_Column3.HeaderText = "理由";
+            this.Reason_Column3.MinimumWidth = 6;
+            this.Reason_Column3.Name = "Reason_Column3";
+            this.Reason_Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Reason_Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Reason_Column3.Width = 251;
+            // 
+            // Check_Column4
+            // 
+            this.Check_Column4.HeaderText = "";
+            this.Check_Column4.MinimumWidth = 6;
+            this.Check_Column4.Name = "Check_Column4";
+            this.Check_Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Check_Column4.Width = 40;
+            // 
+            // Reason_Column4
+            // 
+            this.Reason_Column4.HeaderText = "理由";
+            this.Reason_Column4.MinimumWidth = 6;
+            this.Reason_Column4.Name = "Reason_Column4";
+            this.Reason_Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Reason_Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Reason_Column4.Width = 251;
             // 
             // SelectErrorReason
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(106)))));
-            this.ClientSize = new System.Drawing.Size(571, 531);
+            this.ClientSize = new System.Drawing.Size(701, 588);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.pnlRight);
             this.Controls.Add(this.pnlLeft);
@@ -176,6 +242,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SelectErrorReason";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "理由選択";
             this.Load += new System.EventHandler(this.SelectErrorReason_Load);
             this.Shown += new System.EventHandler(this.SelectErrorReason_Shown);
@@ -194,8 +261,14 @@
         private System.Windows.Forms.Panel pnlRight;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnOK;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn Check;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Reason;
         private MyDataGridView dgvMstDecisionReason;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check_Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason_Column1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check_Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason_Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check_Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason_Column3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check_Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reason_Column4;
     }
 }
--- AirbagApp/ImageChecker/SelectErrorReason.cs
+++ AirbagApp/ImageChecker/SelectErrorReason.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
 using static ImageChecker.Common;
 
@@ -12,6 +13,8 @@ namespace ImageChecker
 
         // パラメータ
         private bool m_bolMainReason = false;
+        private int m_intMaxRowCount = 8;
+        private int m_intMaxColumnCount = 6;
 
         #region メソッド
         /// <summary>
@@ -26,6 +29,13 @@ namespace ImageChecker
             InitializeComponent();
 
             this.StartPosition = FormStartPosition.CenterParent;
+
+            if (m_bolMainReason)
+            {
+                m_intMaxColumnCount = 8;
+                this.Size = new Size(this.Size.Width + 291, this.Size.Height);
+                btnOK.Location = new Point(btnOK.Location.X + 145, btnOK.Location.Y);
+            }
         }
         #endregion
 
@@ -40,11 +50,29 @@ namespace ImageChecker
             this.SuspendLayout();
 
             bool bolProcOkNg = false;
-
+            bool bolInputDecisionReason = false;
+            int intDataRowIndex = 0;
+            int intTargetColumnIndex = 0;
+            string strDecisionReason = string.Empty;
             string strSQL = string.Empty;
             DataTable dtData = new DataTable();
 
-            dgvMstDecisionReason.Rows.Clear();
+            this.dgvMstDecisionReason.Rows.Clear();
+
+            // グリッドの行数・列数を指定
+            this.dgvMstDecisionReason.ColumnCount = m_intMaxColumnCount;
+            this.dgvMstDecisionReason.RowCount = m_intMaxRowCount;
+
+            if (m_bolMainReason)
+            {
+                // 主要のNG理由内容を設定する
+                this.dgvMstDecisionReason.Rows[0].Cells[1].Value = g_CON_NG_REASON_OK;
+                this.dgvMstDecisionReason.Rows[1].Cells[1].Value = g_CON_NG_REASON_WHITE_THREAD_ONE;
+                this.dgvMstDecisionReason.Rows[2].Cells[1].Value = g_CON_NG_REASON_WHITE_THREAD_MULTI;
+                this.dgvMstDecisionReason.Rows[3].Cells[1].Value = g_CON_NG_REASON_BLACK_THREAD_ONE;
+                this.dgvMstDecisionReason.Rows[4].Cells[1].Value = g_CON_NG_REASON_BLACK_THREAD_MULTI;
+                this.dgvMstDecisionReason.Rows[5].Cells[1].Value = g_CON_NG_REASON_OTHER_NG_JUDGEMENT;
+            }
 
             try
             {
@@ -57,35 +85,46 @@ namespace ImageChecker
 
                 g_clsConnectionNpgsql.SelectSQL(ref dtData, strSQL);
 
-                // データグリッドビューに反映
-                // 主要
-                if (m_bolMainReason == true)
+                for (int intColumnIndex = 0; intColumnIndex < m_intMaxColumnCount; intColumnIndex++)
                 {
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_OK });
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_WHITE_THREAD_ONE });
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_WHITE_THREAD_MULTI });
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_BLACK_THREAD_ONE });
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_BLACK_THREAD_MULTI });
-                    this.dgvMstDecisionReason.Rows.Add(new object[] { 0, g_CON_NG_REASON_OTHER_NG_JUDGEMENT });
-                }
+                    // チェックボックス列をスキップ
+                    intColumnIndex++;
 
-                // その他
-                foreach (DataRow dr in dtData.Rows)
-                {
-                    // 主要が含まれていたら除く
-                    switch (dr["decision_reason"].ToString())
+                    for (int intRowIndex = 0; intRowIndex < m_intMaxRowCount; intRowIndex++)
                     {
-                        case g_CON_NG_REASON_OK:
-                        case g_CON_NG_REASON_WHITE_THREAD_ONE:
-                        case g_CON_NG_REASON_WHITE_THREAD_MULTI:
-                        case g_CON_NG_REASON_BLACK_THREAD_ONE:
-                        case g_CON_NG_REASON_BLACK_THREAD_MULTI:
-                        case g_CON_NG_REASON_OTHER_NG_JUDGEMENT:
-
+                        // 既にNG理由が設定済みのセルはスキップ
+                        if (this.dgvMstDecisionReason.Rows[intRowIndex].Cells[intColumnIndex].Value != null)
+                        {
                             continue;
-                    }
+                        }
 
-                    this.dgvMstDecisionReason.Rows.Add(dr.ItemArray);
+                        bolInputDecisionReason = false;
+                        strDecisionReason = string.Empty;
+
+                        // DBから移送が必要なレコードを特定する
+                        while (!bolInputDecisionReason && dtData.Rows.Count > intDataRowIndex)
+                        {
+                            strDecisionReason = dtData.Rows[intDataRowIndex]["decision_reason"].ToString();
+                            bolInputDecisionReason = CheckNGReason(strDecisionReason);
+                            intDataRowIndex++;
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(strDecisionReason))
+                        {
+                            this.dgvMstDecisionReason.Rows[intRowIndex].Cells[intColumnIndex].Value = strDecisionReason;
+                        }
+                        else
+                        {
+                            intTargetColumnIndex = intColumnIndex - 1;
+
+                            // 移送するレコードが存在しない場合、チェックボックスを編集不可とする
+                            DataGridViewCell cell = this.dgvMstDecisionReason.Rows[intRowIndex].Cells[intTargetColumnIndex];
+                            DataGridViewCheckBoxCell chkCell = cell as DataGridViewCheckBoxCell;
+                            chkCell.FlatStyle = FlatStyle.Flat;
+                            chkCell.Style.ForeColor = Color.DarkGray;
+                            chkCell.ReadOnly = true;
+                        }
+                    }
                 }
 
                 bolProcOkNg = true;
@@ -93,7 +132,7 @@ namespace ImageChecker
             catch (Exception ex)
             {
                 // ログ出力
-                WriteEventLog(g_CON_LEVEL_ERROR,string.Format("{0}{1}{2}", g_clsMessageInfo.strMsgE0001 ,Environment.NewLine, ex.Message));
+                WriteEventLog(g_CON_LEVEL_ERROR, string.Format("{0}{1}{2}", g_clsMessageInfo.strMsgE0001, Environment.NewLine, ex.Message));
                 // メッセージ出力
                 System.Windows.Forms.MessageBox.Show(g_clsMessageInfo.strMsgE0030, g_CON_MESSAGE_TITLE_ERROR, MessageBoxButtons.OK, MessageBoxIcon.Error);
 
@@ -102,7 +141,9 @@ namespace ImageChecker
             finally
             {
                 if (bolProcOkNg == false)
+                {
                     this.Close();
+                }
 
                 this.ResumeLayout();
             }
@@ -126,34 +167,109 @@ namespace ImageChecker
         /// <param name="e"></param>
         private void btnOK_Click(object sender, EventArgs e)
         {
-            // 選択行の品番をプロパティにセットする
-            foreach (DataGridViewRow row in dgvMstDecisionReason.Rows)
+            // 選択行のNG理由をプロパティにセットする
+            for (int intColumnIndex = 0; intColumnIndex < m_intMaxColumnCount; intColumnIndex++)
             {
-                if (row.Cells[0].Value.Equals(true))
+                for (int intRowIndex = 0; intRowIndex < m_intMaxRowCount; intRowIndex++)
                 {
-                    strDecisionReason = row.Cells[1].Value.ToString();
-                    this.Close();
+                    if (this.dgvMstDecisionReason.Rows[intRowIndex].Cells[intColumnIndex].Value.Equals(true))
+                    {
+                        strDecisionReason = this.dgvMstDecisionReason.Rows[intRowIndex].Cells[++intColumnIndex].Value.ToString();
+                        this.Close();
+                        return;
+                    }
                 }
+
+                // 理由列をスキップ
+                intColumnIndex++;
             }
         }
 
         /// <summary>
-        /// 行選択イベント
+        /// セル選択イベント
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void dgvMstDecisionReason_RowEnter(object sender, DataGridViewCellEventArgs e)
+        private void dgvMstDecisionReason_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-            // 選択行にチェックを入れる
-            dgvMstDecisionReason.Rows[e.RowIndex].Cells[0].Value = true;
+            int intTargetCheckIndex = e.ColumnIndex;
+            int intTargetReasonIndex = e.ColumnIndex;
 
-            // 選択行以外のチェックを外す
-            for (int rowIndex = 0; rowIndex < dgvMstDecisionReason.Rows.Count; rowIndex++)
+            if (e.ColumnIndex % 2 == 1)
             {
-                if (rowIndex != e.RowIndex)
+                // インデックスをチェックボックス列へ補正する
+                intTargetCheckIndex--;
+            }
+            else
+            {
+                // インデックスを理由列へ補正する
+                intTargetReasonIndex++;
+            }
+
+            // 理由未設定のセルはスキップする
+            if (dgvMstDecisionReason.Rows[e.RowIndex].Cells[intTargetReasonIndex].Value == null)
+            {
+                return;
+            }
+
+            // 全てのチェックを外す
+            for (int intColumnIndex = 0; intColumnIndex < m_intMaxColumnCount; intColumnIndex++)
+            {
+                for (int intRowIndex = 0; intRowIndex < m_intMaxRowCount; intRowIndex++)
                 {
-                    dgvMstDecisionReason.Rows[rowIndex].Cells[0].Value = false;
+                    this.dgvMstDecisionReason.Rows[intRowIndex].Cells[intColumnIndex].Value = false;
                 }
+
+                // 理由列をスキップ
+                intColumnIndex++;
+            }
+
+            // 選択行にチェックを入れる
+            dgvMstDecisionReason.Rows[e.RowIndex].Cells[intTargetCheckIndex].Value = true;
+        }
+
+        /// <summary>
+        /// セル編集イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgvMstDecisionReason_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            int intTargetReasonIndex = e.ColumnIndex;
+
+            if (e.ColumnIndex % 2 == 0)
+            {
+                // インデックスを理由列へ補正する
+                intTargetReasonIndex++;
+            }
+
+            // 理由未設定のセルはスキップする
+            if (dgvMstDecisionReason.Rows[e.RowIndex].Cells[intTargetReasonIndex].Value == null)
+            {
+                e.Cancel = true;
+            }
+        }
+        #endregion
+
+        #region 内部メソッド
+        /// <summary>
+        /// NG内容チェック
+        /// </summary>
+        /// <param name="strNGReason">NG内容</param>
+        private bool CheckNGReason(string strNGReason)
+        {
+            // グリッドに追加するNG内容をチェックする
+            switch (strNGReason)
+            {
+                case g_CON_NG_REASON_OK:
+                case g_CON_NG_REASON_WHITE_THREAD_ONE:
+                case g_CON_NG_REASON_WHITE_THREAD_MULTI:
+                case g_CON_NG_REASON_BLACK_THREAD_ONE:
+                case g_CON_NG_REASON_BLACK_THREAD_MULTI:
+                case g_CON_NG_REASON_OTHER_NG_JUDGEMENT:
+                    return false;
+                default:
+                    return true;
             }
         }
         #endregion
